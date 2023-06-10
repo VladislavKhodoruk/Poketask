d6391f1f656dbf6ff3c03161a3645b2d1f5997ad
Vladislav <Vladislav@DESKTOP-NDQQ267>
2023-06-10 15:37:49 +0300

    Add project files.

---
+++ Poketask/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Poketask;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Poketask/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Poketask;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
